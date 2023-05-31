5826da292c8adbfffd105f174f4f7c48a3b0b247
goott04 <goott04@DESKTOP-47PU9N9>
2023-05-31 10:32:23 +0900

    commit

---
+++ J002/src/com/tech/w01/TypeClass.java
@@ -0,0 +1,21 @@
+package com.tech.w01;
+
+public class TypeClass {
+	public static void main(String[] args) {
+//		사용자 타입 참조형 타입
+		
+		String var1="가";//문자열 홍길동
+		var1="가";
+		char c='a';//char는 한 글자 뜻이고 한 글자는 ''로 표시
+		
+		int var2=200;
+		boolean flag=true;//boolean은 yes or no일 때 쓰임
+		boolean onOff=false;//false는 0인데 0이 아닌 다른 숫자는 true라고 인식되기도 함.
+		
+		System.out.println(var1);
+		System.out.println(c);
+		System.out.println(var2);
+		System.out.println(flag);
+		System.out.println(onOff);
+	}
+}
---
+++ J002/src/com/tech/w02/UnicodeExam.java
@@ -0,0 +1,21 @@
+//package com.tech.w02;
+//
+//public class UnicodeExam {
+//	public static void main(String[] args) {
+////		�����ڵ�����
+//		char char1='��';
+//		char uni1='\uac00';
+//		System.out.println(char1);
+//		System.out.println(uni1);
+//
+//		char uni2='\uc2e0';
+//		char uni3='\uc7ac';
+//		char uni4='\ud658';
+//		
+//		System.out.println(uni2);
+//		System.out.println(uni3);
+//		System.out.println(uni4);
+//		System.out.println("hihi " +uni2+uni3+uni4);
+//	}
+//}
+// 
